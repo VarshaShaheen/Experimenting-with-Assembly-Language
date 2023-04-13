597727eb19feaf6c2b007b7db94d90aa4a5e1726
VarshaShaheen <varshashaheen2003@gmail.com>
2023-04-13 22:19:46 +0530

    program 1 print

---
+++ 1_print.asm
@@ -0,0 +1,18 @@
+section .data
+    msg: db "Hello, World!", 10
+    msg_len equ $ - msg
+
+section .text
+ global _start
+  _start:
+    mov rax, 1  ; specify which system call to invoke, 1-write system call
+    mov rdi, 1  ; specify the file descriptor, 1-stdout
+    mov rsi, msg ; specify the address of the message
+    mov rdx, msg_len   ; specify the length of the message
+    syscall
+    call exit
+
+exit:
+    mov rax, 60 ; specify which system call to invoke, 60-exit system call
+    mov rdi, 0  ; specify the exit code, 0-success
+    syscall
